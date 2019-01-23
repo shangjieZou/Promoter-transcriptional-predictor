338d86205e66f3ce2e0c6fc3c95672e139a6a79a
shangjieZou <31823702+shangjieZou@users.noreply.github.com>
2019-01-23 20:50:15 +0800

    Update Vectorizing-Sequences.py

--- source-code/Vectorizing-Sequences.py
+++ source-code/Vectorizing-Sequences.py
@@ -1,7 +1,8 @@
 import re
+import matplotlib.pyplot as plt
 
 
-
+# This block is used to transform .txt into lists with pure sequences
 def convert_txt_to_pure_seq(string_seqs):
     new_string = string_seqs
     new_string.strip("BBa")
@@ -9,8 +10,6 @@ def convert_txt_to_pure_seq(string_seqs):
     pure_seqs = re.findall(pattern, string_seqs)
     pure_seq = list(set(pure_seqs))
     return pure_seq
-
-
 
 
 def positive_seq_list():
@@ -36,17 +35,43 @@ def Negative_seq_list():
     f_Negative_seq.close()
     return list_seq_negative
 
-# 刚想到，可以用matplotlib查看一下序列长度的大致分布情况，然后考虑下一步
 
-"""
-def max_len(list_seq_positive, list_seq_constitutive, list_seq_negative):
-    max_positive, max_constitutive, max_negative = 0, 0, 0
-    for i in range(len(list_seq_positive)):
-        max_positive = max(max_positive, len(list_seq_positive[i]))
-    for i in range(len(list_seq_constitutive)):
-        max_constitutive = max(max_constitutive, len(list_seq_constitutive[i]))
-    for i in range(len(list_seq_negative)):
-        max_negative = max(max_negative, len(list_seq_negative[i]))
-    max_len = max(max_constitutive, max_negative, max_positive)
-    print max_len
-"""
+
+
+# This block is for screening seqs which are oversize and plot distribution of seq length
+def plot_distrbution_of_seq_len(list_seq_positive, list_seq_constitutive, list_seq_negative):
+    def accumulate_seq_len(list_seq):
+        x = []
+        for i in range(len(list_seq)):
+            x.append(len(list_seq[i]))
+        return x
+    x_positive = accumulate_seq_len(list_seq_positive)
+    x_constitutive = accumulate_seq_len(list_seq_constitutive)
+    x_negative = accumulate_seq_len(list_seq_negative)
+    plt.hist(x_positive,histtype='stepfilled', alpha=0.4, density=6, bins=25, color="navy", label="Positive-regulated promoter")
+    plt.hist(x_constitutive, histtype='stepfilled', alpha=0.4, density=6, bins=25, color="green", label="Constitutive promoter")
+    plt.hist(x_negative, histtype='stepfilled', alpha=0.4, density=6, bins=25, color="darkgoldenrod", label="Repressible promoter")
+    plt.ylabel("Frequency/Class Interval")
+    plt.xlabel("Length of Sequences")
+    plt.title("Distribution of Sequences' Length")
+    plt.legend()
+    plt.show()
+
+def litering_oversize_seqs(list_seq):
+    litered_seq_list = []
+    for i in range(len(list_seq)):
+        if len(list_seq[i]) <= 200:
+            # if liter len_seq<=200, len(positive)=57, len(constitutive)=42, len(negative)=102
+            litered_seq_list.append(list_seq[i])
+    return litered_seq_list
+
+def main_plot_distrbution():
+    list_seq_positive = positive_seq_list()
+    list_seq_constitutive = constitutive_seq_list()
+    list_seq_negative = Negative_seq_list()
+    liter_list_seq_positive = litering_oversize_seqs(list_seq_positive)
+    liter_list_seq_constitutive = litering_oversize_seqs(list_seq_constitutive)
+    liter_list_seq_negative = litering_oversize_seqs(list_seq_negative)
+    plot_distrbution_of_seq_len(liter_list_seq_positive, liter_list_seq_constitutive, liter_list_seq_negative)
+
+
