9fee39cd470eafbec4a80ab82d8b4b915b930ac8
shangjieZou <31823702+shangjieZou@users.noreply.github.com>
2019-01-24 21:16:57 +0800

    Add files via upload

---
+++ source-code/SVM Model.py
@@ -0,0 +1,5 @@
+from VectorizingSequences import Vectoring_main
+
+vectorized_positive_list = Vectoring_main()[0]
+vectorized_constitutive_list = Vectoring_main()[1]
+vectorized_negative_list = Vectoring_main()[2]
