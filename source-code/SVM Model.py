21ceede07a8fee46e4d3706851759a61ef63125b
shangjieZou <31823702+shangjieZou@users.noreply.github.com>
2019-02-03 23:07:57 +0800

    Update SVM Model.py

--- source-code/SVM Model.py
+++ source-code/SVM Model.py
@@ -1,57 +1,98 @@
-from VectorizingSequences import Vectoring_main
+from Try_new_vectoring_method_with_dict import main_convert
 import numpy as np
 from sklearn.model_selection import cross_val_score,train_test_split
 from sklearn import svm
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
-vectorized_positive_list = Vectoring_main()[0]
-vectorized_constitutive_list = Vectoring_main()[1]
-vectorized_negative_list = Vectoring_main()[2]
+vectorized_positive_dict = main_convert()[0]
+vectorized_constitutive_dict = main_convert()[1]
+vectorized_negative_dict = main_convert()[2]
+
+def filter_oversize(dict_seq, threshold):
+    list_newdict_key = []
+    list_newdict_value = []
+    for key in dict_seq.keys():
+        list_newdict_key.append(key)
+        list_newdict_value.append(dict_seq[key])
+    newdict = dict(zip(list_newdict_key, list_newdict_value))
+    for key in dict_seq.keys():
+        if len(dict_seq[key]) > threshold*4:
+            del newdict[key]
+    return newdict
+
+reduced_vectorized_positive_dict = filter_oversize(vectorized_positive_dict, 500)  # len = 89
+reduced_vectorized_constitutive_dict = filter_oversize(vectorized_constitutive_dict, 500) # len = 83
+reduced_vectorized_negative_dict = filter_oversize(vectorized_negative_dict, 500)  # len = 203
 
 
 # fill seq vectors with 0
-def fill_zero(list_seq):
-    for i in range(len(list_seq)):
+def fill_zero(dict_seq):
+    for key in dict_seq.keys():
         list_zero = [0]
-        list_seq[i] += (800-len(list_seq[i]))*list_zero
-    return list_seq
+        dict_seq[key] += (500*4-len(dict_seq[key]))*list_zero
+    return dict_seq
 
-vectorized_positive_list_final = fill_zero(vectorized_positive_list)
-vectorized_constitutive_list_final = fill_zero(vectorized_constitutive_list)
-vectorized_negative_list_final = fill_zero(vectorized_negative_list)
+
+# fill seqs to equal length
+vectorized_positive_dict_final = fill_zero(reduced_vectorized_positive_dict)
+vectorized_constitutive_dict_final = fill_zero(reduced_vectorized_constitutive_dict)
+vectorized_negative_dict_final = fill_zero(reduced_vectorized_negative_dict)
+
+vectorized_positive_list_final = [vectorized_positive_dict_final[key]
+                                      for key in vectorized_positive_dict_final.keys()]
+vectorized_constitutive_list_final = [vectorized_constitutive_dict_final[key]
+                                      for key in vectorized_constitutive_dict_final.keys()]
+vectorized_negative_list_final = [vectorized_negative_dict_final[key]
+                                      for key in vectorized_negative_dict_final.keys()]
 
 
 # transform to numpy array and divide training set as well as test set
 Vector_whole = vectorized_constitutive_list_final + vectorized_positive_list_final + vectorized_negative_list_final
-X = np.array(Vector_whole).reshape(201, 800)
-y_list_constitutive = 42*[0]
-y_list_regulated = 159*[1]
-#y_list_positive = 57*[1]
-#y_list_negative = 102 * [2]
+X = np.array(Vector_whole).reshape(375, 2000)
+y_list_constitutive = 83*[0]
+y_list_inducible = 292*[1]
+#y_list_positive = 89*[1]
+#y_list_negative = 203 * [2]
 
 #y_list = y_list_positive+y_list_constitutive+y_list_negative
-y_list = y_list_constitutive + y_list_regulated
-y = np.array(y_list).reshape(201)
+y_list = y_list_constitutive + y_list_inducible
+y = np.array(y_list).reshape(375)
 
-def normal_training():
-    train_X,test_X,train_y,test_y = train_test_split(X,y,test_size=0.2,random_state=1)
 
-    C = 0.5
-    list_C = []
-    list_acc = []
-    max_acc = 0
-    max_C = 0
-    while C<12.0:
-        predictive_model = svm.SVC(C=C, kernel='rbf', gamma='auto')
-        predictive_model.fit(train_X, train_y)
-        acc = cross_val_score(predictive_model, test_X, test_y, cv = 10)
-        list_C.append(C)
-        list_acc.append(acc)
+#def normal_training():
+train_X,test_X,train_y,test_y = train_test_split(X,y,test_size=0.2,random_state=1)
 
-    plt.plot(list_C, list_acc)
-    plt.xlabel("Parameter C")
-    plt.ylabel("acc")
-    plt.title("Optimizing Parameter C")
-    plt.show()
+C = 0.5
+list_C = []
+list_acc = []
+while C<48:
+    #promoter_svm = svm.SVC(kernel='rbf', gamma="auto", C=C)
+    #promoter_svm.fit(train_X, train_y)
+    #predict_y = promoter_svm.predict(test_X)
+    #acc = accuracy_score(test_y, predict_y)
+    predictive_model = svm.SVC(C=C, kernel='rbf', gamma='auto')
+    predictive_model.fit(train_X, train_y)
+    acc_cross = cross_val_score(predictive_model, test_X, test_y, cv = 7)
+    acc = sum(acc_cross)/7
+    list_C.append(C)
+    list_acc.append(acc)
+    C += 0.5
 
-normal_training()
+
+print(list_C)
+print(list_acc)
+max_acc = max(list_acc)
+dict_training = dict(zip(list_acc, list_C))
+max_C = dict_training[max_acc]
+print("max_C = "+str(max_C))
+print("max_acc = " + str(max_acc))
+
+
+
+plt.plot(list_C, list_acc, linewidth = '1', color='coral', linestyle=':', marker='|')
+plt.xlabel("Parameter C")
+plt.ylabel("ACC")
+plt.title("Optimizing Parameter C")
+plt.show()
+
