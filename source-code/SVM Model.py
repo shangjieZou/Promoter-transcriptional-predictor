5936d180fa06c00e5484d8bdfec46993d2fcf302
shangjieZou <31823702+shangjieZou@users.noreply.github.com>
2019-02-04 22:18:02 +0800

    Update SVM Model.py

--- source-code/SVM Model.py
+++ source-code/SVM Model.py
@@ -3,7 +3,10 @@ import numpy as np
 from sklearn.model_selection import cross_val_score,train_test_split
 from sklearn import svm
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
 
 vectorized_positive_dict = main_convert()[0]
 vectorized_constitutive_dict = main_convert()[1]
@@ -52,47 +55,124 @@ Vector_whole = vectorized_constitutive_list_final + vectorized_positive_list_fin
 X = np.array(Vector_whole).reshape(375, 2000)
 y_list_constitutive = 83*[0]
 y_list_inducible = 292*[1]
-#y_list_positive = 89*[1]
-#y_list_negative = 203 * [2]
+# y_list_positive = 89*[1]
+# y_list_negative = 203 * [2]
 
-#y_list = y_list_positive+y_list_constitutive+y_list_negative
+# y_list = y_list_positive+y_list_constitutive+y_list_negative
 y_list = y_list_constitutive + y_list_inducible
 y = np.array(y_list).reshape(375)
 
 
-#def normal_training():
 train_X,test_X,train_y,test_y = train_test_split(X,y,test_size=0.2,random_state=1)
 
-C = 0.5
-list_C = []
-list_acc = []
-while C<48:
-    #promoter_svm = svm.SVC(kernel='rbf', gamma="auto", C=C)
-    #promoter_svm.fit(train_X, train_y)
-    #predict_y = promoter_svm.predict(test_X)
-    #acc = accuracy_score(test_y, predict_y)
-    predictive_model = svm.SVC(C=C, kernel='rbf', gamma='auto')
+
+def optimizing_C():
+    C = 0.5
+    list_C = []
+    list_acc = []
+    while C<48:
+        predictive_model = svm.SVC(C=C, kernel='rbf', gamma='auto')
+        predictive_model.fit(train_X, train_y)
+        acc_cross = cross_val_score(predictive_model, test_X, test_y, cv = 7, scoring="accuracy")
+        acc = sum(acc_cross)/7
+        list_C.append(C)
+        list_acc.append(acc)
+        C += 0.5
+    print(list_C)
+    print(list_acc)
+    max_acc = max(list_acc)
+    dict_training = dict(zip(list_acc, list_C))
+    max_C = dict_training[max_acc]
+    print("max_C = "+str(max_C))
+    print("max_acc = " + str(max_acc))
+
+    plt.plot(list_C, list_acc, linewidth = '1', color='coral', linestyle=':', marker='|')
+    plt.xlabel("Parameter C")
+    plt.ylabel("Accuracy")
+    plt.title("Optimizing Parameter C")
+    plt.show()
+
+
+def optimizing_gamma():
+    C = 10
+    gamma = 0.01
+    list_gamma = []
+    list_acc = []
+    while gamma<10:
+        predictive_model = svm.SVC(C=C, kernel='rbf', gamma= gamma)
+        predictive_model.fit(train_X, train_y)
+        acc_cross = cross_val_score(predictive_model, test_X, test_y, cv = 7, scoring="accuracy")
+        acc = sum(acc_cross)/7
+        list_gamma.append(gamma)
+        list_acc.append(acc)
+        gamma += 0.01
+    print(list_gamma)
+    print(list_acc)
+    max_acc = max(list_acc)
+    dict_training = dict(zip(list_acc, list_gamma))
+    max_gamma = dict_training[max_acc]
+    print("max_gamma = "+str(max_gamma))
+    print("max_acc = " + str(max_acc))
+
+    plt.plot(list_gamma, list_acc, linewidth = '1', color='coral', linestyle=':', marker='|')
+    plt.xlabel("Parameter gamma")
+    plt.ylabel("Accuracy")
+    plt.title("Optimizing Parameter gamma")
+    plt.show()
+
+
+
+
+def try_svm_optimized_para():
+    C = 10
+    gamma = 0.05
+    predictive_model = svm.SVC(C=C, kernel='rbf', gamma= gamma)
     predictive_model.fit(train_X, train_y)
-    acc_cross = cross_val_score(predictive_model, test_X, test_y, cv = 7)
+    acc_cross = cross_val_score(predictive_model, test_X, test_y, cv = 7, scoring="accuracy")
     acc = sum(acc_cross)/7
-    list_C.append(C)
-    list_acc.append(acc)
-    C += 0.5
+    print("acc = " + str(acc))
 
 
-print(list_C)
-print(list_acc)
-max_acc = max(list_acc)
-dict_training = dict(zip(list_acc, list_C))
-max_C = dict_training[max_acc]
-print("max_C = "+str(max_C))
-print("max_acc = " + str(max_acc))
+"""
+def plot_optimzing_three_dim():
+    C = 0
+    gamma = 0.05
+    C = np.arange(0.5,48,0.5)
+    list_gamma = []
+    list_acc = []
+    for i in range(len(C)):
+        C_i = C[i]
+        while gamma < 6:
+            predictive_model = svm.SVC(C=C_i, kernel='rbf', gamma=gamma)
+            predictive_model.fit(train_X, train_y)
+            acc_cross = cross_val_score(predictive_model, test_X, test_y, cv=7, scoring='accuracy')
+            acc = sum(acc_cross) / 7
+            list_gamma.append(gamma)
+            list_acc.append(acc)
+            gamma += 0.05
 
-
-
-plt.plot(list_C, list_acc, linewidth = '1', color='coral', linestyle=':', marker='|')
-plt.xlabel("Parameter C")
-plt.ylabel("ACC")
-plt.title("Optimizing Parameter C")
-plt.show()
-
+    max_acc = max(list_acc)
+    dict_training_gamma = dict(zip(list_acc, list_gamma))
+    dict_training_C = dict(zip(list_acc, C))
+    max_gamma = dict_training_gamma[max_acc]
+    max_C = dict_training_C[max_acc]
+    print("C:")
+    print(C)
+    print("list_gamma:")
+    print(list_gamma)
+    print("list_acc:")
+    print(list_acc)
+    print("max_acc = " + str(max_acc))
+    print("max_gamma =a " + str(max_gamma))
+    print("max_C = " + str(max_C))
+    print(len(list_acc))
+    print(len(list_gamma))
+    print(len(C))
+    Z = np.zeros((len(list_acc),3))
+    for i in range(len(list_acc)):
+        Z[i][0] = C[i]
+        Z[i][1] = list_gamma[i]
+        Z[i][2] = list_acc[i]
+    Z.tofile("acc_across.dat", sep=',')
+    print(Z)
+"""
